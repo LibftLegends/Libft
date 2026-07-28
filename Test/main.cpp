18463c478a68906459d68bb571e1d3e66eadca43
Adyem <Bastianvangenechten@hotmail.com>
2026-07-28 22:11:15 +0200

    Avoid Windows filesystem startup crash

--- Test/main.cpp
+++ Test/main.cpp
@@ -18,26 +18,13 @@ namespace
 
     static test_path test_find_libft_root(const test_path &executable_path)
     {
-        std::error_code error_code;
-        const test_path current_path = std::filesystem::current_path(
-            error_code);
-        if (!error_code && std::filesystem::is_directory(current_path / "Test")
-            && std::filesystem::is_directory(current_path / "Modules"))
-        {
-            return (current_path);
-        }
-
         const test_path executable_parent = executable_path.parent_path();
         if (!executable_parent.empty()
             && executable_parent.filename() == "Test")
         {
             return (executable_parent.parent_path());
         }
-        if (current_path.filename() == "Test")
-        {
-            return (current_path.parent_path());
-        }
-        return (current_path);
+        return (test_path("."));
     }
 
     static void test_cleanup_runtime_artifacts(const test_path &root_path)
@@ -61,14 +48,15 @@ int main(int argc, char **argv)
 {
     std::error_code error_code;
     test_path executable_path;
+    test_path root_path;
     // TEMP_DIAGNOSTIC_REMOVE: Delete main-entry tracing after diagnosis.
     ft_test_runner_write_startup_diagnostic("main:entered\n");
     if (argc > 0 && argv != NULL && argv[0] != NULL)
     {
-        executable_path = std::filesystem::absolute(argv[0], error_code);
+        executable_path = test_path(argv[0]);
     }
-    const test_path root_path = test_find_libft_root(executable_path);
     const int test_status = ft_run_registered_tests();
+    root_path = test_find_libft_root(executable_path);
     test_cleanup_runtime_artifacts(root_path);
     return (test_status);
 }
