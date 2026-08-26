222b823cfe540b78e18f806074c150cbd8d9b161
Adyem <Bastianvangenechten@hotmail.com>
2026-08-26 14:27:48 +0200

    Synchronize path step coordinate getters

--- Modules/Game/game_pathfinding.cpp
+++ Modules/Game/game_pathfinding.cpp
@@ -329,20 +329,47 @@ int32_t game_path_step::set_z(ft_size_t z) noexcept
 
 ft_size_t game_path_step::get_x() const noexcept
 {
+    ft_bool lock_acquired;
+    int32_t lock_error;
+    ft_size_t value;
+
     errno_abort_if_uninitialised_or_destroyed(this->_initialised_state, "game_path_step::get_x");
-    return (this->_x);
+    lock_error = this->lock_internal(&lock_acquired);
+    if (lock_error != FT_ERR_SUCCESS)
+        return (0U);
+    value = this->_x;
+    (void)this->unlock_internal(lock_acquired);
+    return (value);
 }
 
 ft_size_t game_path_step::get_y() const noexcept
 {
+    ft_bool lock_acquired;
+    int32_t lock_error;
+    ft_size_t value;
+
     errno_abort_if_uninitialised_or_destroyed(this->_initialised_state, "game_path_step::get_y");
-    return (this->_y);
+    lock_error = this->lock_internal(&lock_acquired);
+    if (lock_error != FT_ERR_SUCCESS)
+        return (0U);
+    value = this->_y;
+    (void)this->unlock_internal(lock_acquired);
+    return (value);
 }
 
 ft_size_t game_path_step::get_z() const noexcept
 {
+    ft_bool lock_acquired;
+    int32_t lock_error;
+    ft_size_t value;
+
     errno_abort_if_uninitialised_or_destroyed(this->_initialised_state, "game_path_step::get_z");
-    return (this->_z);
+    lock_error = this->lock_internal(&lock_acquired);
+    if (lock_error != FT_ERR_SUCCESS)
+        return (0U);
+    value = this->_z;
+    (void)this->unlock_internal(lock_acquired);
+    return (value);
 }
 
 int32_t game_path_step::get_error() const noexcept
